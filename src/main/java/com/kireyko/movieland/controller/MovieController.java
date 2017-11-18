fbe544408a9e25e7fd4d3dd90fe4b41dfd689479
Kireyko Vladimir <kireyko@gmail.com>
2017-11-18 23:20:12 +0200

    ua implementation, currently not working

--- src/main/java/com/kireyko/movieland/controller/MovieController.java
+++ src/main/java/com/kireyko/movieland/controller/MovieController.java
@@ -4,11 +4,11 @@ import com.kireyko.movieland.entity.Genre;
 import com.kireyko.movieland.entity.Movie;
 import com.kireyko.movieland.service.MovieService;
 import com.kireyko.movieland.util.JsonJacksonConverter;
-import com.kireyko.movieland.util.JsonManualConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -18,37 +18,20 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/v1")
+@RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE, method = RequestMethod.GET)
+//produces = "application/json;charset=utf-8"
 public class MovieController {
-    private int movieid = 0;
-    private int genreId =32;
     private final Logger log = LoggerFactory.getLogger(getClass());
-
-    private static final String COMMA_SEPARATOR = ",";
-    private static final String COLON_SEPARATOR = ":";
-    private static final String LINE_SEPARATOR = System.lineSeparator();
 
     @Autowired
     private MovieService movieService;
 
     @Autowired
-    private JsonManualConverter jsonConverter;
-
-    @Autowired
     private JsonJacksonConverter jsonJacksonConverter;
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String welcome(ModelMap model) {
-        model.addAttribute("movieid", ++movieid);
-        model.addAttribute("genreId", genreId);
-
-        log.debug("[movie_] movieid : {}", movieid);
-        return "v1";
-    }
-
-    @RequestMapping(value = "/movie", method = RequestMethod.GET, produces = "application/json;charset=utf-8" )
     @ResponseBody
-    public ResponseEntity<String> getMoviesAll(@RequestParam(required = false) Map<String, String> parameters) {
+    @RequestMapping(value = "/movie")
+    public String getMoviesAll(@RequestParam(required = false) Map<String, String> parameters) {
         log.info("Sending request to get list of movies {} ",parameters);
         long startTime = System.currentTimeMillis();
         String moviesAllJson = null;
@@ -57,11 +40,11 @@ public class MovieController {
             moviesAllJson = jsonJacksonConverter.parseEntityToJson(movies);
         }
         log.info(" List of movies was received. It took {} ms."+System.lineSeparator()+" List: {}", System.currentTimeMillis() - startTime, moviesAllJson);
-        return new ResponseEntity<>(moviesAllJson, HttpStatus.OK);
+        return moviesAllJson;
     }
 
-    @RequestMapping(value = "/movie/random", method = RequestMethod.GET, produces = "application/json;charset=utf-8" )
     @ResponseBody
+    @RequestMapping(value = "/movie/random")
     public ResponseEntity<String> getMoviesRandom() {
         log.info("Sending request to get random list of movies ");
         long startTime = System.currentTimeMillis();
@@ -74,8 +57,8 @@ public class MovieController {
         return new ResponseEntity<>(moviesRandomJson, HttpStatus.OK);
     }
 
-    @RequestMapping(value="/movie/{movieId}", method = RequestMethod.GET, produces = "application/json;charset=utf-8" )
     @ResponseBody
+    @RequestMapping(value="/movie/{movieId}", method = RequestMethod.GET, produces = "application/json;charset=utf-8" )
     public ResponseEntity<String> getMovieById(@PathVariable int movieId) {
         log.info("Sending request to get movie with id = {}", movieId);
         long startTime = System.currentTimeMillis();
@@ -86,8 +69,8 @@ public class MovieController {
         return new ResponseEntity<>(movieJson, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/genre", method = RequestMethod.GET, produces = "application/json;charset=utf-8" )
     @ResponseBody
+    @RequestMapping(value = "/movie/genre")
     public ResponseEntity<String> getGenresAll() {
         log.info("Sending request to get all list of genres ");
         long startTime = System.currentTimeMillis();
@@ -100,9 +83,8 @@ public class MovieController {
         return new ResponseEntity<>(genresAllJson, HttpStatus.OK);
     }
 
-
-    @RequestMapping(value="/movie/genre/{genreId}", method = RequestMethod.GET, produces = "application/json;charset=utf-8" )
     @ResponseBody
+    @RequestMapping(value="/movie/genre/{genreId}" )
     public ResponseEntity<String> getMoviesByGenreId(@PathVariable int genreId) {
         log.info("Sending request to get list of movies by genre id = {}", genreId);
         long startTime = System.currentTimeMillis();
--- src/main/java/com/kireyko/movieland/dao/jdbc/MovieDaoImpl.java
+++ src/main/java/com/kireyko/movieland/dao/jdbc/MovieDaoImpl.java
@@ -13,12 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
-@Service
+//@Service
+@Repository
 public class MovieDaoImpl implements MovieDao{
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final MovieRowMapper movieRowMapper = new MovieRowMapper();
